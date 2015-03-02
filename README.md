835f30192ffd3a9c9bbb3127bb9974483b946c48
Ryan Sturmer <ryansturmer@gmail.com>
2015-03-01 23:59:27 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# em.js
+em.js
+=====
 Javascript shim that fills in as-yet unimplemented text measurement functionality in HTML5 canvas.
+
+What's this about
+-----------------
+Stop me if you've heard this one.  There is __actually no way__ to determine the height of rendered text in the HTML5 canvas API.  The proposed behavior of the [TextMetrics:https://developer.mozilla.org/en-US/docs/Web/API/TextMetrics] API looks promising, but at the time of this writing is almost completely unsupported in almost every browser.
+
+How to use
+----------
+em.js is just a shim that replaces the `measureText` method on the HTML5 canvas 2D drawing context with one that actually implements some of the proposed metrics.  To use it, simply include em.js in your web project before you make any calls to `measureText`
+
+References
+----------
+This was mainly inspired by [this answer:http://stackoverflow.com/questions/1134586/how-can-you-find-the-height-of-text-on-an-html-canvas/9847841#9847841] on stack overflow, which details a method of essentially setting up tht text and rendering it, and using CSS to determine the various text metrics.
+
+ * https://developer.mozilla.org/en-US/docs/Web/API/TextMetrics
+ * https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+ * http://stackoverflow.com/questions/1134586/how-can-you-find-the-height-of-text-on-an-html-canvas/9847841#9847841
+
+Footnote
+--------
+This is by a wide margin, the most boring thing I've ever written.  It hurts me even to document it, but this is one of those rare instances when I did a quick search for this function, and didn't find _any_ canned solution for it, so I feel some obligation to post it online.  I hope that you find it useful.
---
+++ em.js
@@ -0,0 +1,72 @@
+// Copy off the old measurement function (we'll need it)
+CanvasRenderingContext2D.prototype._measureText = CanvasRenderingContext2D.prototype.measureText;
+
+// This was inspired by StackOverflow answer:
+// http://stackoverflow.com/questions/11452022/measure-text-height-on-an-html5-canvas-element
+CanvasRenderingContext2D.prototype.measureText = function(text) {
+	metrics = this._measureText(text);
+
+	var text = document.createElement('span');
+	text.innerHTML = 'Hg';
+	text.style['font'] = this.font
+	
+	var block = document.createElement("div");
+	block.style['display'] = 'inline-block';
+	block.style['width'] = '1px';
+	block.style['height'] = '0px';
+
+	var div = document.createElement('div');
+	div.appendChild(text);
+	div.appendChild(block);
+
+	var body = document.body;
+	body.appendChild(div);
+
+	var ascent = -1;
+	var descent = -1;
+	var height = -1;
+
+	try {
+		block.style['vertical-align'] = 'baseline';
+		ascent = block.offsetTop - text.offsetTop;
+		block.style['vertical-align'] = 'bottom';
+		height = block.offsetTop - text.offsetTop;
+		descent = height - ascent;
+	} finally {
+		document.body.removeChild(div);
+	}
+
+	new_metrics = {}
+
+	// TODO This doesn't account for locale, and is guaranteed broken for those that read right-to-left
+	switch(this.textAlign) {
+		case "start":
+		case "left":
+			new_metrics.actualBoundingBoxLeft = 0;
+			new_metrics.actualBoundingBoxRight = metrics.width;
+			break;
+
+		case "end":
+		case "right":
+			new_metrics.actualBoundingBoxLeft = -metrics.width;
+			new_metrics.actualBoundingBoxRight = 0;
+			break;
+
+		case "center":
+			// TODO This is probably just an approximation.
+			new_metrics.actualBoundingBoxLeft = -metrics.width/2.0;
+			new_metrics.actualBoundingBoxRight = metrics.width/2.0;
+			break;
+	}
+	new_metrics.actualBoundingBoxAscent = ascent;
+	new_metrics.actualBoundingBoxDescent = descent;
+
+	// Copy the new metrics over, if and only if the CanvasRenderingContext2D API doesn't provide them
+	for(key in new_metrics) {
+		if(!(key in metrics)) {
+			metrics[key] = new_metrics[key];
+		}
+	}
+
+	return metrics;
+}
